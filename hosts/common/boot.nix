36e811a02169262102471fa600dd37eb33212193
System administrator <root@davidFramework.com>
2025-01-28 14:39:56 -0700

    new grub load page

--- hosts/common/boot.nix
+++ hosts/common/boot.nix
@@ -1,13 +1,20 @@
 {
-# use grub as the boot loader. UEFI mdoe only. BIOS is dead.
-# An alternative is systemd-boot
-	boot.loader = {
-		efi.canTouchEfiVariables = true;
-		grub = {
-			enable = true;
-			device = "nodev";
-			efiSupport = true;
-			configurationLimit = 10; # only store 10 configurations
-		};
-	};
+  # use grub as the boot loader. UEFI mdoe only. BIOS is dead.
+  # An alternative is systemd-boot
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      configurationLimit = 10; # only store 10 configurations
+
+      minegrub-theme = {
+        enable = true;
+        splash = "Flakey";
+        background = "background_options/1.20 - [Trails & Tales].png";
+        boot-options-count = 4;
+      };
+    };
+  };
 }
