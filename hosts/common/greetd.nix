d28954cf8600bc89e172ff58b279203bd22bb44e
Aiden <aiden.dor@student.nmt.edu>
2026-06-11 00:49:11 -0600

    Remove swaylock from greetd.nix - already in sway module and basePackages

--- hosts/common/greetd.nix
+++ hosts/common/greetd.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, config, ... }:
+{ lib, pkgs, ... }:
 let
   cfg = config.hosts.common.greetd;
 in {
@@ -20,57 +20,6 @@ in {
     users.users.greeter = {
       isSystemUser = true;
       description = "Greeter User";
-    };
-
-    environment.systemPackages = [
-      pkgs.swaylock-effects
-    ];
-
-    programs.swaylock = {
-      enable = false;
-      settings = {
-        effect-bg = "1e1e2e";
-        effect-color = "cdd6f4";
-        image = config.stylix.image;
-        inside-color = "313244";
-        inside-clear-color = "f38ba8";
-        inside-correct-color = "a6e3a1";
-        inside-wrong-color = "f38ba8";
-        key-hl-color = "89b4fa";
-        layout-bg-color = "313244";
-        layout-text-color = "cdd6f4";
-        line-color = "313244";
-        line-clear-color = "f38ba8";
-        line-correct-color = "a6e3a1";
-        line-wrong-color = "f38ba8";
-        ring-color = "313244";
-        ring-clear-color = "f38ba8";
-        ring-correct-color = "a6e3a1";
-        ring-wrong-color = "f38ba8";
-        separator-color = "00000000";
-        text-color = "cdd6f4";
-        text-clear-text = "INCORRECT";
-        text-correct-text = "CORRECT";
-        text-wrong-text = "INCORRECT";
-        fading = true;
-        fade-in = 0.3;
-        grayscale = false;
-        show-failed-attempts = true;
-        timer = true;
-        indicator = true;
-        indicator-cylinder-color = "313244";
-        indicator-ring-color = "cdd6f4";
-        indicator-text-color = "cdd6f4";
-        clock = true;
-        time-size = 72;
-        month-size = 48;
-        date-size = 48;
-        weekday-size = 48;
-        time-color = "cdd6f4";
-        date-color = "cdd6f4";
-        weekday-color = "cdd6f4";
-        font = "DejaVu Sans Mono";
-      };
     };
   };
 }
