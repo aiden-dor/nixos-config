ea6db72bcfc24666eaf2ca60f1a18993f8c6f507
Aiden <aiden.dor@student.nmt.edu>
2026-06-11 00:43:02 -0600

    Remove time format variables from tuigreet command

--- hosts/common/greetd.nix
+++ hosts/common/greetd.nix
@@ -11,7 +11,7 @@ in {
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '🕰  ${t} on ${d}' --cmd Sway";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Sway";
           user = "greeter";
         };
       };
